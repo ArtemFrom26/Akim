37c5d4e850c5dc90d25b70cccb1beb28ac64ac42
Artem <maslennikov.ae@phystech.edu>
2022-07-16 11:19:52 +0300

    Akim ti debil

--- tasks_easy/tasks1.cpp
+++ tasks_easy/tasks1.cpp
@@ -1,32 +1,33 @@
-// Ввожу тебе int, должен вывости все разряды числа. Пример: ВВОД: 123 ВЫВОД: 100, 20, 3.
-
-#include<iostream>
+#include <iostream>
 
 int discharge(int number) {
     int temp = 1;
     int n = 0;
-    while((number / temp) != 0) {
+    while ((number / temp) != 0) {
         temp *= 10;
         n++;
     }
-    return n;
+    
+	return n;
 }
 
 int my_pow(int n, int m) {
     int temp = n;
-    for(int i = 1; i <= m; i++) n *= temp;
-    return n;
+    for (int i = 1; i <= m; i++) {
+		n *= temp;
+	}
+    
+	return n;
 }
 
 int main() {
     int number;
     std::cin >> number;
-    int discharge;
-    discharge = discharge(number);
+    int dd = discharge(number);
 
-    while(discharge != 0) {
-        std::cin << (number / my_pow(10, discharge - 1)) * (discharge - 1) << std::endl;
-        number = number % (discharge - 1);
-        discharge--;
+    while (dd != 0) {
+        std::cout << (number / my_pow(10, dd - 1)) * (dd - 1) << '\n';
+        number = number % (dd - 1);
+        --dd;
     }
 }
