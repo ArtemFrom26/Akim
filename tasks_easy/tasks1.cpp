609134225c265245c6d31b772fe0209a117716b1
Akimk <akim.druzhinin7@yandex.ru>
2022-07-16 18:26:56 +0300

    Чуть подсмотрел как сделать возведение в степень рекурсией

--- tasks_easy/tasks1.cpp
+++ tasks_easy/tasks1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int discharge(int number) {
+int digit(int number) {
     int temp = 1;
     int n = 0;
     while ((number / temp) != 0) {
@@ -12,22 +12,24 @@ int discharge(int number) {
 }
 
 int my_pow(int n, int m) {
-    int temp = n;
-    for (int i = 1; i <= m; i++) {
-		n *= temp;
-	}
-    
-	return n;
+    if (m == 0) {
+        return 1;
+    }
+
+    else {
+        return n*my_pow(n, m-1);
+    }
+
 }
 
 int main() {
-    int number;
-    std::cin >> number;
-    int dd = discharge(number);
+    int num;
+    std::cin >> num;
+    int dd = digit(num);
 
-    while (dd != 0) {
-        std::cout << (number / my_pow(10, dd - 1)) * (dd - 1) << '\n';
-        number = number % (dd - 1);
-        --dd;
+    while(dd != 0) {
+        std::cout << ((num / (my_pow(10,(dd - 1)))) * (my_pow(10,(dd - 1)))) << std::endl;
+        num = (num % (my_pow(10, (dd - 1))));
+        dd--;
     }
 }
